9ca71b570dccb44314e368e6529e4dc64248685a
Carson <carsonh@gmail.com>
2022-09-25 16:59:30 -0400

    Sanity Schemas with errors

--- CH.Portfolio/schemas/schema.js
+++ CH.Portfolio/schemas/schema.js
@@ -3,7 +3,15 @@ import createSchema from 'part:@sanity/base/schema-creator'
 
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
+import works from './works'
 import testimonials from './testimonials'
+import brands from './brands'
+import abouts from './abouts'
+import experiences from './experiences'
+import skills from './skills'
+import WorkExperience from './Workexperience'
+import contact from "./contact"
+
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
