36ed303ef7d9667fa2aefc491d7be1532c081f02
Carson <carsonh@gmail.com>
2022-09-26 04:36:53 -0400

    Sanity Schemas working

--- CH.Portfolio/schemas/schema.js
+++ CH.Portfolio/schemas/schema.js
@@ -1,22 +1,30 @@
-// First, we must import the schema creator
+// Import the schema creator
 import createSchema from 'part:@sanity/base/schema-creator'
 
-// Then import schema types from any plugins that might expose them
+// Import schema types and categories
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 import works from './works'
 import testimonials from './testimonials'
 import brands from './brands'
-import abouts from './abouts'
+import about from './about'
 import experiences from './experiences'
 import skills from './skills'
-import WorkExperience from './Workexperience'
+import workExperience from './workExperience'
 import contact from "./contact"
 
 
-// Then we give our schema to the builder and provide the result to Sanity
+// Pass schemas over to Sanity
 export default createSchema({
   name: 'default',
   types: schemaTypes.concat([,
-  testimonials
+  testimonials,
+  works,
+  brands,
+  about,
+  experiences,
+  skills,
+  workExperience,
+  contact
+
   ]),
 })
