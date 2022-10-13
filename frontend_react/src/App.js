27ca2792b82db2da5afef21045ebaee2e38dfe3a
Carson Hall <carsonh@gmail.com>
2022-10-13 15:20:25 -0400

    App.JS work

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -3,11 +3,14 @@ import React from 'react';
 
 // Import containers from index.js
 import {About, Footer, Header, Skills, Testimonial, Work} from './container';
+// Import components from index.js
+import { Navbar } from './components';
 
 // const App = () => {
   function App () {
   return (
     <div className="app">
+      <Navbar />
       <Header />
       <About />
      <Work />
